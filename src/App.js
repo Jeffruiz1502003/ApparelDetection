e06eb33cbc5e45f80a21ac56863ab86ad4b7286f
Jeffruiz1502003 <123280099+Jeffruiz1502003@users.noreply.github.com>
2023-09-25 15:44:58 -0400

    adding changes

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ loadUser = (data) => {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://mybackend-lqtr.onrender.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -90,7 +90,7 @@ loadUser = (data) => {
           .then(response => response.json())
           .then(response => {
             if (response) {
-              fetch('http://localhost:3000/image', {
+              fetch('https://mybackend-lqtr.onrender.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
