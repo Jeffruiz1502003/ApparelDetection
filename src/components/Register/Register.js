440434439a7c9c9429a75849e3828a5e30393228
Jeffruiz1502003 <123280099+Jeffruiz1502003@users.noreply.github.com>
2023-10-02 18:34:43 -0400

    Adding further changes to register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -31,16 +31,15 @@ class Register extends React.Component {
         name: this.state.name
       })
     })
-    .then(response => response.json())
-    .then(users => {
-      if (users.id) {
-        this.props.loadUser(users)
+    .then(res => res.json())
+    .then(user => {
+      if (user.id) {
+        this.props.loadUser(user)
         this.props.onRouteChange('home');
       }
     })
   }
   render() {
-    const { onRouteChange } = this.props
     return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
